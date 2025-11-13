498d130d778c12deb94a50c0a16671e8c6872bc8
Michael Becker <21beckem8@gmail.com>
2025-11-13 12:26:41 -0700

    Navbar is finally working and starting to make colors.

--- client/src/components/Head.svelte
+++ client/src/components/Head.svelte
@@ -2,4 +2,5 @@
 <meta charset="UTF-8" />
 <link rel="icon" type="image/svg+xml" href="/images/favicon.ico" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="css/styles.css">
 <title>Dragon's Quill</title>
--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -1,9 +1,26 @@
-<img src="/images/DragonsQuill.png" alt="Logo">
-<h1>Dragon's Quill</h1>
+<div>
+    <img src="/images/DragonQuill.png" alt="Logo">
+    <h1>Dragon's Quill</h1>
+</div>
 
 <style>
 
-    img{
-        width: 60px;
+    div{
+        display: flex;
+        flex-direction: row;
+        position: absolute;
+        background-color: var(--primary-color);
+        width: 100vw;
+        top: 0;
+        left: 0;
     }
+
+    img{
+        max-width: 60px;
+    }
+
+    h1{
+        color: var(--font-color);
+    }
+    
 </style>
