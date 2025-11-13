591ea1eb53f44a8a7d3f68b2ebb2e16d70614ee7
Michael Becker <21beckem8@gmail.com>
2025-11-13 12:12:55 -0700

    Home page loads and can mount components!

--- client/src/js/utils.js
+++ client/src/js/utils.js
@@ -5,12 +5,13 @@ import Head from '../components/Head.svelte'
 export function mountComponent(component, targetSelector) {
     let target = document.querySelector(targetSelector);
     if (target) {
-        mount(component, target);
+        mount(component, {
+            target
+        });
     }
 }
 
 export function mountOnEveryPage() {
-    debugger;
     mountComponent(Head, 'head');
     mountComponent(Nav, 'body > nav');
 }
