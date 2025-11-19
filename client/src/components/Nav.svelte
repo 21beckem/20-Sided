68c79d3e4adccfbe1c8832b4c3ec4f42a4b99939
Michael Becker <21beckem8@gmail.com>
2025-11-19 10:40:46 -0700

    Made adjustments to clerk login scroll style

--- client/src/components/Nav.svelte
+++ client/src/components/Nav.svelte
@@ -33,40 +33,46 @@
 <div class="row">
     <img src="/images/DragonQuill.png" alt="Logo">
     <h1>Dragon's Quill</h1>
-    <div class="user-btn" bind:this={signInBtn}></div>
+    <div style="
+    position: absolute;
+    right: 2rem;
+    cursor: pointer;" bind:this={signInBtn}></div>
 </div>
 
 <style>
 
-    div.row{
-        display: flex;
-        flex-direction: row;
-        position: absolute;
-        background-color: var(--primary-color);
-        width: 100vw;
-        top: 0;
-        left: 0;
-        align-items: center;
-        color: var(--font-color);
-        font-weight: bold;
-    }
+div.row {
+    display: flex;
+    flex-direction: row;
+    position: absolute;
+    background-color: var(--primary-color);
+    width: 100vw;
+    top: 0;
+    left: 0;
+    align-items: center;
+    color: var(--font-color);
+    font-weight: bold;
+}
 
-    img{
-        max-width: 60px;
-    }
+img {
+    margin-left: 1rem;
+    max-width: 60px;
+}
 
-    h1{
-        text-shadow:
-            -1px -1px 0 #000,
-            1px -1px 0 #000,
-            -1px 1px 0 #000,
-            1px 1px 0 #000;
-    }
+h1 {
+    margin-left: 1rem;
+    font-family: var(--scroll-font);
+    text-shadow:
+        -1px -1px 0 #000,
+        1px -1px 0 #000,
+        -1px 1px 0 #000,
+        1px 1px 0 #000;
+}
 
-    .user-btn{
-        position: absolute;
-        right: 2rem;
-        cursor: pointer;
-    }
-    
+.user-btn {
+    position: absolute;
+    right: 2rem;
+    cursor: pointer;
+}
+
 </style>
