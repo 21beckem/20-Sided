1d57029e2ecee83c57caac8327d62f3da3561bd9
Michael Becker <21beckem8@gmail.com>
2025-11-24 11:16:19 -0700

    Creating new map now works

--- server/routes/map.routes.js
+++ server/routes/map.routes.js
@@ -4,9 +4,47 @@ const router = Router();
 
 // `POST /maps` - Create new map
 router.post('/', async (req, res) => {
-    res.status(200).json({
-        worked: true
-    });
+    if (!req.body || !req.body.json) {
+        res.status(400).json({
+            worked: false,
+            error: 'Missing JSON'
+        });
+        return;
+    }
+    
+    let json = false;
+    try {
+        json = JSON.parse(req.body.json);
+        
+    } catch (e) {
+        res.status(400).json({
+            worked: false,
+            error: 'Invalid JSON'
+        });
+        return;
+    }
+
+    if (!json.map) {
+        res.status(400).json({
+            worked: false,
+            error: 'Missing map'
+        });
+        return;
+    }
+    if (json.map.children.length === 0) {
+        res.status(400).json({
+            worked: false,
+            error: 'Map must have at least one chunk'
+        });
+        return;
+    }
+
+    // set type based on number of chunks
+    json.type = json.map.children.length > 1 ? 'map' : 'chunk';
+
+    let result = await mongodb.getDb().collection('maps').insertOne(json);
+
+    res.status(200).json(result);
 });
 
 // `GET /maps/:id` - Get specific map
@@ -15,11 +53,7 @@ router.get('/:id', async (req, res) => {
     let result = await mongodb.getDb().collection('maps').findOne({
         _id: new mongodb.ObjectId(id)
     });
-    res.status(200).json({
-        worked: true,
-        id: id,
-        result: result
-    });
+    res.status(200).json(result);
 });
 
 // - `PUT /maps/:id` - Update map
@@ -33,6 +67,3 @@ router.put('/:id', async (req, res) => {
 
 
 export default router; // Export the router to use it in the main file
-
-
-//(map/chunk type will be determined by counting number of chunks)
