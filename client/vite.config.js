4c74294430e9d92463afbd5b47d9e6a9f6450d62
Michael Becker <21beckem8@gmail.com>
2025-12-01 10:33:17 -0700

    started doing stuff

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  appType: 'mpa', // show error message (404) instead of home page if route not found
   plugins: [svelte()],
   root: 'src/',
   server: {
+    host: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
