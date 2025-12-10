5c83fcaa0fd2888789a321d1b2539b86b23d2dcf
wpeterashworth <wingsaseagles12@gmail.com>
2025-12-10 11:57:03 -0700

    Updated vite config for render

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   appType: 'mpa', // show error message (404) instead of home page if route not found
   plugins: [svelte()],
   root: 'src/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  },
   server: {
     host: true,
     allowedHosts: ["two0-sided.onrender.com"],
