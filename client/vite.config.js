71e5417556e652448e189c81c4d2dd1b0bb54c4c
wpeterashworth <wingsaseagles12@gmail.com>
2025-12-10 12:09:10 -0700

    fixed vite config again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +9,16 @@ export default defineConfig({
   root: 'src/',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        browse: resolve(__dirname, 'src/browse/index.html'),
+        builder: resolve(__dirname, 'src/builder/index.html'),
+        details: resolve(__dirname, 'src/details/index.html'),
+        profile: resolve(__dirname, 'src/profile/index.html')
+      }
+    }
   },
   server: {
     host: true,
